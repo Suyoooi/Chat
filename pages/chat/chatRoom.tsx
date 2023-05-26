d9bcadf2d3ed16d75c1d39bfd0e0a275fb89ed33
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-26 12:53:30 +0900

    fix: publish 추가

--- pages/chat/chatRoom.tsx
+++ pages/chat/chatRoom.tsx
@@ -1,12 +1,31 @@
 import { useEffect, useRef, useState } from "react";
 import { TextField, Button, List, ListItem } from "@mui/material";
 import { Client, IMessage } from "@stomp/stompjs";
+import * as StompJs from "@stomp/stompjs";
+
+interface ChatMessage {
+  body: string;
+}
 
 const ChatRoom = () => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
   const socket = useRef<Client | null>(null);
   const subscription = useRef<any | null>(null);
+  const [chatList, setChatList] = useState<ChatMessage[]>([]);
+  const [chat, setChat] = useState("");
+  const client = useRef<StompJs.Client | null>(null);
+
+  const connect = () => {
+    client.current = new StompJs.Client({
+      brokerURL: "ws://192.168.10.183:8080/ws",
+      onConnect: () => {
+        console.log("success");
+        subscribe();
+      },
+    });
+    client.current.activate();
+  };
 
   useEffect(() => {
     socket.current = new Client();
@@ -16,7 +35,7 @@ const ChatRoom = () => {
       onConnect: () => {
         console.log("성공했습니다");
         subscription.current = socket.current?.subscribe(
-          "/topic/messages",
+          "/sub/messages",
           (message) => {
             const receivedMessage = JSON.parse(message.body).content;
             setMessages((prevMessages) => [...prevMessages, receivedMessage]);
@@ -36,38 +55,70 @@ const ChatRoom = () => {
     };
   }, []);
 
-  const sendMessage = () => {
-    if (message && socket.current?.connected) {
-      socket.current.publish({
-        destination: "/app/chat",
-        body: JSON.stringify({ content: message }),
-      });
-      setMessage("");
-    }
+  // const sendMessage = () => {
+  //   if (message && socket.current?.connected) {
+  //     socket.current.publish({
+  //       destination: "/app/chat",
+  //       body: JSON.stringify({ content: message }),
+  //     });
+  //     setMessage("");
+  //   }
+  // };
+  const publish = (chat: string) => {
+    if (!client.current?.connected) return;
+
+    client.current.publish({
+      destination: "/pub/sendMessage",
+    });
+
+    setChat("");
   };
+
+  const subscribe = () => {
+    client.current?.subscribe("/sub/1", (body) => {
+      const json_body = JSON.parse(body.body);
+      console.log(json_body);
+      // setChatList((_chat_list) => [
+      // ..._chat_list, json_body
+      // ]);
+    });
+  };
+
+  const disconnect = () => {
+    client.current?.deactivate();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 채팅 입력 시 state에 값 설정
+    setChat(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent, chat: string) => {
+    // 보내기 버튼 눌렀을 때 publish
+    e.preventDefault();
+
+    publish(chat);
+  };
+
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  }, []);
 
   return (
     <div>
-      <List>
-        {messages.map((msg, index) => (
-          <ListItem key={index}>{msg}</ListItem>
-        ))}
-      </List>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          sendMessage();
-        }}
-      >
-        <TextField
-          label="Message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          variant="outlined"
-        />
-        <Button type="submit" variant="contained" color="primary">
-          Send
-        </Button>
+      <div className="chat-list"></div>
+      <form onSubmit={(event) => handleSubmit(event, chat)}>
+        <div>
+          <input
+            type="text"
+            name="chatInput"
+            onChange={handleChange}
+            value={chat}
+          />
+        </div>
+        <input type="submit" value="의견 보내기" />
       </form>
     </div>
   );
